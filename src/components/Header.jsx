bac09343c978e0e77fda98b046324ce35fe8fc25
srikanth <srikanth@finzy.com>
2020-04-28 13:34:55 +0530

    Adding page count

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ export default class Header extends React.Component {
             <div className="app-header">
                 <span className="header-text">
                     Algoritms for all 👼 👩‍💻 👨‍💼 👩‍🚀 🤴 🧛‍♀️ 🧟‍♂️ 🧞‍♂️ 🧜‍♀️ 🧚‍♂️ 💀 .
+                    <img src="https://hitwebcounter.com/counter/counter.php?page=7284071&style=0007&nbdigits=5&type=page&initCount=0" title="User Stats" Alt="PHP Hits Count"   border="0" ></img>
                 </span>
             </div>
         )
