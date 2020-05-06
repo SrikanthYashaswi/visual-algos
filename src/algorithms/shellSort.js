742f381eaf5913fafc2559129aff424f1758d1a0
icoder18 <deekshaamadan@gmail.com>
2020-05-06 21:29:25 +0530

    adding shell sort

---
+++ src/algorithms/shellSort.js
@@ -0,0 +1,17 @@
+const shellSort=(list)=>{
+    let changes=[];
+    changes.push({ list: [...list], indexChanges: [0, 0] });
+    let gap=Math.floor(list.length/2);
+    for(let i=gap;i>0;i=Math.floor(i/2)){
+        for(let j=i;j<list.length;j++){
+            let temp=list[j];
+            let k;
+            for(k=j;k>=i && list[k-i]>temp;k=k-i){
+                list[k]=list[k-i];
+            }
+            list[k]=temp;
+            changes.push({ list: [...list], indexChanges: [i, k] });
+        }
+    }
+    return changes;
+}
