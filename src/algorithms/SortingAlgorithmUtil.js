8c77fc78865d091ff2fc514764b16b3e475cb339
Timotej Avsec <avsectimotej1@gmail.com>
2020-06-10 21:39:04 +0200

    Add Binary Insertion Sort implementation

--- src/algorithms/SortingAlgorithmUtil.js
+++ src/algorithms/SortingAlgorithmUtil.js
@@ -3,13 +3,15 @@ import { insertionSort } from "./insertionSort";
 import { mergeSort } from './mergeSort';
 import { bubbleSort } from './bubbleSort';
 import { shellSort } from './shellSort';
+import { binaryInsertionSort } from "./binaryInsertionSort";
 
 export const sortingAlgorithms = {
     SELECTION_SORT: 'selection-sort',
     INSERTION_SORT: 'insertion-sort',
     MERGE_SORT: 'merge-sort',
     BUBBLE_SORT: 'bubble-sort',
-    SHELL_SORT: 'shell-sort'
+    SHELL_SORT: 'shell-sort',
+    BINARY_INSERTION_SORT: 'binary-insertion-sort'
 }
 
 export const getSortEventsForAlgorithm = (algorithm, list) => {
@@ -28,6 +30,9 @@ export const getSortEventsForAlgorithm = (algorithm, list) => {
         }
         case sortingAlgorithms.SHELL_SORT: {
             return shellSort(list);
+        }
+        case sortingAlgorithms.BINARY_INSERTION_SORT: {
+            return binaryInsertionSort(list);
         }
         default: {
             return []
